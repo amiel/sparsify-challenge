f40a6f8bf836f311eb0c221d1b7bdd23571c7d74
Amiel Martin <amiel.martin@gmail.com>
2013-10-23 20:14:12 -0700

    A first attempt at step 1

--- lib/sparsify.rb
+++ lib/sparsify.rb
@@ -1,5 +1,16 @@
 # encoding: utf-8
 
 module Sparsify
-  # Your implementation goes here
+  def self.sparse(source)
+    source.each_with_object(Hash.new) do |(key, value), new_hash|
+      if value.is_a? Hash
+        sparse(value).each do |k, v|
+          new_hash["#{key}.#{k}"] = v
+        end
+      else
+        new_hash[key] = value
+      end
+    end
+  end
+
 end
